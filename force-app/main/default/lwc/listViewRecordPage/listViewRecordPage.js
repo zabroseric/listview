9e71e82b8f6c07b7618f1abfd6ab897845463d58
Shaun Ripley <shaun.ripley@smartapps.co.nz>
2022-05-02 11:30:15 +1200

    Add the ability to subscribe to change events on record pages

--- force-app/main/default/lwc/listViewRecordPage/listViewRecordPage.js
+++ force-app/main/default/lwc/listViewRecordPage/listViewRecordPage.js
@@ -1,4 +1,11 @@
 import {api, LightningElement} from 'lwc';
+import getSObjectNameApex from '@salesforce/apex/ListViewController.getSObjectName'
+import {logApexFunc} from "c/utils";
+import {subscribe} from 'lightning/empApi';
+import userId from "@salesforce/user/Id";
+
+// Wrap the apex functions using a decorative pattern for logging.
+const getSObjectName = logApexFunc('getSObjectName', getSObjectNameApex);
 
 export default class ListViewRecordPage extends LightningElement {
 
@@ -15,6 +22,8 @@ export default class ListViewRecordPage extends LightningElement {
 
   @api recordId;
 
+  hasRendered = false;
+
   /**
    * Modify the SOQL passed by injecting the record id, before passing it
    * to the list view for processing.
@@ -23,5 +32,29 @@ export default class ListViewRecordPage extends LightningElement {
    */
   get soqlModified() {
     return (this.soql || '').replace(/'?:?recordid'?/gi, `'${this.recordId}'`);
+  }
+
+  renderedCallback() {
+    if (!this.hasRendered) {
+      this.onSubscribe().catch((e) => console.error(e));
+      this.hasRendered = true;
+    }
+  }
+
+  async onSubscribe() {
+    const sObjectChangeEvent = await this.getsObjectChangeEvent();
+
+    const messageCallback = (event) => {
+      if (event.data.payload.CreatedById === userId) {
+        this.template.querySelector('c-list-view').refreshData();
+      }
+    }
+    console.debug(`Subscribing to: ${sObjectChangeEvent}`);
+    subscribe(sObjectChangeEvent, -1, messageCallback).catch((e) => console.error(e));
+  }
+
+  async getsObjectChangeEvent() {
+    const sObjectName = await getSObjectName({sObjectName: this.template.querySelector('c-list-view').sObjectName});
+    return !sObjectName.includes('__c') ? `/data/${sObjectName}ChangeEvent` : `/data/${sObjectName}__ChangeEvent`;
   }
 }
