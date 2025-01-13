25752086a1979ccab7e6c1e1dd2b479a1a971c75
Shaun Ripley <github@zabro.co.nz>
2025-01-13 16:09:43 +1100

    Implement user_mode + move cardheading to card

--- force-app/main/default/lwc/card/card.js
+++ force-app/main/default/lwc/card/card.js
@@ -4,9 +4,10 @@ export default class Card extends LightningElement {
 
   @api title;
   @api titleCount;
-  @api subTitle;
-
   @api iconName;
+  @api subTitle;
+  @api isLoading;
+  @api error;
 
   get hasTitleCount() {
     return this.titleCount || this.titleCount === 0;
